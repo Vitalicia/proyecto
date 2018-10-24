b1b07fd8ed7b19664e7f1ef82ad6e5a3d2c9c9c4
BrayanCorpse <al221610173@gmail.com>
2018-10-23 23:25:00 -0500

    vista alta medicamento

---
+++ resources/views/vitalicia/rMedicamentos.blade.php
@@ -0,0 +1,50 @@
+@extends('vitalicia.principal')
+@section('encabezado')
+<h3>Nuevo Medicamento</h3>
+@stop
+@section('form1')
+        <form action="{{route('gume')}}" method = "POST" enctype="multipart/form-data" >
+            {{csrf_field()}}
+            Clave 
+                <input type="text" name='idmedicamento' value="{{$iddm}}" readonly= 'readonly' >
+          
+
+            Medicamento
+
+            @if($errors->first('nombre')) 
+               <br>{!! $errors->first('nombre','<span class=error>:message</span>')!!}
+          @endif  
+                <input type="text" name="nombre" value="{{old('nombre')}}">
+                
+                   
+
+
+            Indicación 
+            @if($errors->first('indicacion')) 
+            <br>{!! $errors->first('indicacion','<span class=error>:message</span>')!!}
+            @endif  
+                <input type="text" name="indicacion" value="{{old('indicacion')}}">
+                
+            Presentación
+            @if($errors->first('presen')) 
+            <br>{!! $errors->first('presen','<span class=error>:message</span>')!!}
+            @endif
+                <input type="text" name="presen" value="{{old('presen')}}">
+                
+            Terminotx
+            @if($errors->first('terminotx')) 
+            <br>{!! $errors->first('terminotx','<span class=error>:message</span>')!!}
+            @endif
+                <input type="text" name="terminotx" value="{{old('terminotx')}}">
+
+                Seleccione carrera<select name = 'idh'>
+            @foreach($horarios as $hr)
+            <option value = '{{$hr->idh}}'>{{$hr->tipohorario}}</option>
+            @endforeach
+                  </select>
+<br>
+
+            
+            <input type="submit" class="button" value="Guardar">
+            </form>
+@stop
