525c666a77d5e00b4e721a6a67c0e3acb9122d23
yarednoriega <al.221611332@gmail.com>
2018-10-24 14:26:38 -0500

    consultadatos

--- app/datos.php
+++ app/datos.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class datos extends Model
 {
+	use SoftDeletes;
     protected $primaryKey ='idd';
     protected $fillable = ['idd','nombre','ap','am','edad','telefono','calle','numero','calle1','calle2','colonia','municipio','ciudad','cp','referencia','archivo'];
+    protected $date=['deleted_at'];
 
 }
 
