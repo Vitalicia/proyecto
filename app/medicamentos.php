d6a62ee3c614cf5c117f30c06991092a9b36ba74
BrayanCorpse <al221610173@gmail.com>
2018-10-24 09:54:53 -0500

    model

--- app/medicamentos.php
+++ app/medicamentos.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class medicamentos extends Model
 {
-    //
+    protected $primaryKey ='idmedicamento';
+    protected $fillable = ['idmedicamento','nombre','indicacion','presen','terminotx','idh'];
 }
