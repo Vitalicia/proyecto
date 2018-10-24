520f1f016af03043d0f34aa9157c477683e6196c
BrayanCorpse <al221610173@gmail.com>
2018-10-23 23:15:23 -0500

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,10 +21,14 @@ Route::get('/rPaciente','vitalicia@rPaciente')->name('rpac');
 
 Route::get('/cPaciente','vitalicia@cPaciente')->name('cpac');
 
+//GUARDA DATOS DEL MEDICAMENTO
+Route::POST('/guardamedicamento', 'vitalicia@guardamedicamento')->name('gume');
+
 
 //GUARDA DATOS DE USUARIO
 Route::POST('/guardausuario', 'vitalicia@guardausuario')->name('gusu');
 
 //GUARDA LOS DATOS DE SESION DE USUARIO
 Route::POST('/gusuario', 'vitalicia@gusuario')->name('gusua');
+Route::POST('/gumedica', 'vitalicia@gumedica')->name('gumedi')
 
