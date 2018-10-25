f974f89671cc358c805d3f3eebd8e30e044becf9
yarednoriega <al.221611332@gmail.com>
2018-10-25 16:46:35 -0500

    ca

--- resources/views/vitalicia/cdatos.blade.php
+++ resources/views/vitalicia/cdatos.blade.php
@@ -9,20 +9,35 @@
 <h1>Reporte de datos</h1>
 <br>
 <table border= 1>
-<tr><td>ID</td><td>Nombre</td><td>Ap</td>
-<td>Am</td><td>Edad</td></tr>
+<tr><td>ID</td><td>Nombre</td><td>Apellido Paterno</td>
+<td>Apellido materno</td><td>Edad</td><td>Telefono</td><td>Calle</td><td>Numero</td>
+<td>Calle1</td><td>Calle2</td><td>Colonia</td><td>Municipio</td><td>Ciudad</td><td>Cp</td><td>Referencia</td>
+
 @foreach($datosd as $datt)
+
+
 <tr><td>{{$datt->idd}}</td>
-<td>{{$datt->nombre}}</td><td>{{$datt->ap}}</td><td>{{$datt->am}}</td><td>{{$datt->edad}}</td>
-<td> 
-<a href="{{URL::action('vitalicia@getdatos',['idd'=>$datt->idd])}}"> 
-Eliminar  
-</a>
-<a href="{{URL::action('vitalicia@getdatos',['idd'=>$datt->idd])}}"> 
-Modificar 
-</a>
+<td>{{$datt->nombre}}</td>
+<td>{{$datt->ap}}</td>
+<td>{{$datt->am}}</td>
+<td>{{$datt->edad}}</td>
+<td>{{$datt->telefono}}</td>
+<td>{{$datt->calle}}</td>
+<td>{{$datt->numero}}</td>
+<td>{{$datt->calle1}}</td>
+<td>{{$datt->calle2}}</td>
+<td>{{$datt->colonia}}</td>
+<td>{{$datt->municipio}}</td>
+<td>{{$datt->ciudad}}</td>
+<td>{{$datt->cp}}</td>
+
+<td>{{$datt->referencia}}</td>
+
+
+
 </td></tr>
 @endforeach
 </table>
 </body>
 </html>
+
