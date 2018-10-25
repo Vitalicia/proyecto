4b57e8e5ac6186acde9c1c3a3578c169770ab710
BrayanCorpse <al221610173@gmail.com>
2018-10-24 23:15:34 -0500

    new models geriatricos and gvalores

---
+++ app/geriatricos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class geriatricos extends Model
+{
+    protected $primarykey ='idgeriatricos';
+    protected $fillable = ['idgeriatricos','valorg','valorg1','valorg2','idvg'];
+}
---
+++ app/gvalores.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class gvalores extends Model
+{
+    protected $primarykey ='idvg';
+    protected $fillable = ['idvg','tipogvalor'];
+}
