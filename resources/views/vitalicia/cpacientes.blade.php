7a7996c12b06d71eb2739e4cf45e8a9b78146444
yarednoriega <al.221611332@gmail.com>
2018-10-26 00:06:53 -0500

    vistapa

---
+++ resources/views/vitalicia/cpacientes.blade.php
@@ -0,0 +1,43 @@
+<form action = "{{route('getpacientes')}}" method = "POST">
+        {{csrf_field()}}
+        
+        
+        
+        <html>
+<body>
+
+<h1>Reporte de Pacientes</h1>
+<br>
+<table border= 1>
+<tr><td>ID</td>
+<td>Fecha Pacientes</td>
+<td>Apellido Paterno</td>
+<td>Datos</td>
+<td>Medicamentos</td>
+<td>Alimentacion</td>
+<td>Signos</td>
+<td>Geriatricos</td>
+<td>Actividades</td>
+
+
+@foreach($pacientesd as $patt)
+
+
+<tr><td>{{$patt->idpacientes}}</td>
+<td>{{$patt->fechapaciente}}</td>
+<td>{{$patt->idd}}</td>
+<td>{{$patt->idmedicamento}}</td>
+<td>{{$patt->idalimentacion}}</td>
+<td>{{$patt->ids}}</td>
+<td>{{$patt->idgeriatricos}}</td>
+<td>{{$patt->idactividades}}</td>
+
+
+
+
+</td></tr>
+@endforeach
+</table>
+</body>
+</html>
+
