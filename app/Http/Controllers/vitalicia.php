9184d86b56469c341efdff18e00c85dd960e4489
BrayanCorpse <al221610173@gmail.com>
2018-11-05 10:42:11 -0600

    function guardageriatrico

--- app/Http/Controllers/vitalicia.php
+++ app/Http/Controllers/vitalicia.php
@@ -327,12 +327,12 @@ class vitalicia extends Controller
                
            
                 $ger = new geriatricos;
-                $ger->idgeriatricos = $request->input('idvgeriatricos');
-                $ger->valorg = $request->input('valorg');
-                $ger->valorg1 = $request->input('valorg1');
-                $ger->valorg2 = $request->input('valorg2');  
-                $ger->idvg= $request->input('idvg');
-                $ger->save($ger);
+                $ger->idgeriatricos = $request->idgeriatricos;
+                $ger->valorg = $request->valorg;
+                $ger->valorg1 = $request->valorg1;
+                $ger->valorg2 = $request->valorg2;  
+                $ger->idvg= $request->idvg;
+                $ger->save();
 
 
                 return redirect()->route('home');
