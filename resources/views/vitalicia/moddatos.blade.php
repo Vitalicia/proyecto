6d8e14d741f0921138bef0420812c8cc63201a3d
BrayanCorpse <al221610173@gmail.com>
2018-11-12 11:26:45 -0600

    style2

--- resources/views/vitalicia/moddatos.blade.php
+++ resources/views/vitalicia/moddatos.blade.php
@@ -10,23 +10,23 @@
             
             Nombre
             <br>{!! $errors->first('nombre','<span class=error>:message</span>')!!}
-                <input type="text" name="nombre" value="{{$datosm->idd}}">
+                <input type="text" name="nombre" value="{{$datosm->nombre}}">
                 
             Apellido Paterno 
             <br>{!! $errors->first('ap','<span class=error>:message</span>')!!}
-                <input type="text" name="ap" value="{{$datosm->idd}}">
+                <input type="text" name="ap" value="{{$datosm->ap}}">
                 
             Apellido Materno
             <br>{!! $errors->first('am','<span class=error>:message</span>')!!}
-                <input type="text" name="am" value="{{$datosm->idd}}">
+                <input type="text" name="am" value="{{$datosm->am}}">
                 
             Edad 
             <br>{!! $errors->first('edad','<span class=error>:message</span>')!!}
-                <input type="text" name="edad" value="{{$datosm->idd}}">
+                <input type="text" name="edad" value="{{$datosm->edad}}">
 
             Tel&eacute;fono
             <br>{!! $errors->first('telefono','<span class=error>:message</span>')!!}
-                <input type="text" name="telefono" value="{{$datosm->idd}}">
+                <input type="text" name="telefono" value="{{$datosm->telefono}}">
 @stop
 @section('form2')               
             Sexo 
@@ -37,40 +37,40 @@
             <br>
             Calle 
             <br>{!! $errors->first('calle','<span class=error>:message</span>')!!}
-                <input type="text" name="calle" value="{{$datosm->idd}}">
+                <input type="text" name="calle" value="{{$datosm->calle}}">
                  
             N&uacute;mero 
             <br>{!! $errors->first('numero','<span class=error>:message</span>')!!}
-                <input type="text" name="numero" value="{{$datosm->idd}}">
+                <input type="text" name="numero" value="{{$datosm->numero}}">
                 
             Entre calle 
             <br>{!! $errors->first('calle1','<span class=error>:message</span>')!!}
-                <input type="text" name="calle1" value="{{$datosm->idd}}">
+                <input type="text" name="calle1" value="{{$datosm->calle1}}">
                 
             y calle 
             <br>{!! $errors->first('calle2','<span class=error>:message</span>')!!}
-                <input type="text" name="calle2" value="{{$datosm->idd}}">
+                <input type="text" name="calle2" value="{{$datosm->calle2}}">
                 
             Colonia
             <br>{!! $errors->first('colonia','<span class=error>:message</span>')!!}
-                <input type="text" name="colonia" value="{{$datosm->idd}}">
+                <input type="text" name="colonia" value="{{$datosm->colonia}}">
 @stop
 @section('form3')                
             Municipio
             <br>{!! $errors->first('municipio','<span class=error>:message</span>')!!}
-                <input type="text" name="municipio" value="{{$datosm->idd}}">
+                <input type="text" name="municipio" value="{{$datosm->municipio}">
                 
             Ciudad
             <br>{!! $errors->first('ciudad','<span class=error>:message</span>')!!}
-                <input type="text" name="ciudad" value="{{$datosm->idd}}">
+                <input type="text" name="ciudad" value="{{$datosm->ciudad}}">
                 
             C&oacute;digo Postal 
             <br>{!! $errors->first('cp','<span class=error>:message</span>')!!}
-                <input type="text" name="cp" value="{{$datosm->idd}}">
+                <input type="text" name="cp" value="{{$datosm->cp}}">
                 
             Referencia 
             <br>{!! $errors->first('referencia','<span class=error>:message</span>')!!}
-                <input type="text" name="referencia" value="{{$datosm->idd}}">
+                <input type="text" name="referencia" value="{{$datosm->referencia}}">
                 
                 <input type="submit" class="button" value="Guardar">
         </form>
