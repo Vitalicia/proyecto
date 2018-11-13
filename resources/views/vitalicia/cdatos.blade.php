3b4f2b5fb9909bbf869dbb8b86d91e3c5861cc63
victorhdiazz <44076613+victorhdiazz@users.noreply.github.com>
2018-11-12 18:13:35 -0600

    Modificacion a vista de consulta de datos

--- resources/views/vitalicia/cdatos.blade.php
+++ resources/views/vitalicia/cdatos.blade.php
@@ -6,14 +6,15 @@
 
         <div style="overflow-x:auto;">
 <table border= 1>
-<tr><td>ID</td><td>Nombre</td><td>Imagen</td>
+<tr>
+<td>ID</td><td>Nombre</td>
 <td>Apellido Paterno</td><td>Apellido materno</td><td>Edad</td>
 <td>Telefono</td><td>Calle</td><td>Numero</td><td>Calle1</td>
 <td>Calle2</td><td>Colonia</td><td>Municipio</td><td>Ciudad</td>
-<td>Cp</td><td>Referencia</td><td>Operaciones</td>
+<td>Cp</td><td>Referencia</td><td>Foto</td><td>Operaciones</td>
 
 @foreach($datosd as $datt)
-
+</tr>
 
 <tr><td>{{$datt->idd}}</td>
 <td>{{$datt->nombre}}</td>
@@ -30,21 +31,13 @@
 <td>{{$datt->ciudad}}</td>
 <td>{{$datt->cp}}</td>
 <td>{{$datt->referencia}}</td>
-<td><img src = "{{asset('archivos/'.$datt->archivo)}}"
-        height =50 width=50>
-    </td>
+<td><img src = "{{asset('archivos/'.$datt->archivo)}}" height =100 width=100></td>
+<td><a href="{{URL::action('vitalicia@modificadat',['idd'=>$datt->idd])}}">Modificar</a></td>
 
-<td>
 
-  
-   <a href="{{URL::action('vitalicia@modificadat',['idd'=>$datt->idd])}}"> 
-   Modificar</a>
-	
 
-</td>
-</tr>
-
-</tr>
 @endforeach
+</tr>
 </table>
+</div>
 @stop
