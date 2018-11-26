65c352a27045cec10be0bc27179e9de850918ec8
BrayanCorpse <al221610173@gmail.com>
2018-11-25 22:25:33 -0600

    contenido4

--- resources/views/vitalicia/home.blade.php
+++ resources/views/vitalicia/home.blade.php
@@ -120,12 +120,37 @@
     
   <!--Contenido de la Tab 4-->
   <div class="tabs-panel" id="panel4c">
+  <div class="table-scroll">
+        <table class="hover">
+<tr>
+<th>ID</th>
+<th>Medicamento</th>
+<th>Indicacion</th>
+<th>Presentación</th>
+<th>Terminotx</th>
+<th>Horario</th>
+<th>Nmedica</th>
+
+
+@foreach($medicamentosm as $medi)
+</tr>
+
+<tr>
+<td>{{$medi->idpaciente}}</td>
+<td>{{$medi->fechapaciente}}</td>
+<td>{{$medi->nombre}}</td>
+<td>{{$medi->medicamento}}</td>
+<td>{{$medi->alimentacion}}</td>
+<td>{{$medi->signos}}</td>
+<td>{{$medi->valorg}}</td>
+
+
+@endforeach
+</tr>
+</table>
+</div>
+@stop
     
   </div>
     
     
-</div>
-
-   
-            
-@stop
