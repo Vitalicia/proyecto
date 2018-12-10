49b2333a6cb56e90a4bd99a9ad01cd5f8fc73897
victorhdiazz <44076613+victorhdiazz@users.noreply.github.com>
2018-12-09 19:31:22 -0600

    update

--- resources/views/vitalicia/inicio.blade.php
+++ resources/views/vitalicia/inicio.blade.php
@@ -43,7 +43,7 @@
 		        <div class="navbar-header">
 		        	<!-- Mobile Toggle Menu Button -->
 					<a href="#" class="js-insideV-nav-toggle insideV-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
-		          	<a class="navbar-brand" href="http://vitalicia.com.mx"><img src="#" alt="Vitalicia"></a>  
+		          	<a class="navbar-brand" href="http://vitalicia.com.mx"><img src="logo.png" alt="Vitalicia"></a>  
 		        </div>
 		        <div id="navbar" class="navbar-collapse collapse">
 		          <ul class="nav navbar-nav navbar-right">
