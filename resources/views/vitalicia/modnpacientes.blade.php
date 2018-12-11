f7814a82314113224438da1657dc1812b632c436
BrayanCorpse <al221610173@gmail.com>
2018-12-11 13:38:24 -0600

    er<s

--- resources/views/vitalicia/modnpacientes.blade.php
+++ resources/views/vitalicia/modnpacientes.blade.php
@@ -15,11 +15,7 @@
                 @endforeach
                 </select>
             
-            <b>Medicamento</b> <select name = 'medicam'>
-                            @foreach($amedicamentos as $amed)
-                            <option value = '{{$amed->idamedicamento}}'>{{$amed->nmedica}}</option>
-                            @endforeach
-                        </select>
+           
             
             <b>Indicaci&oacute;n</b>
             @if($errors->first('amindicacion')) 
