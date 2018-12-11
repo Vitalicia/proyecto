3ca01b0634d83f93e6127a66a0303ca0bd6895bd
BrayanCorpse <al221610173@gmail.com>
2018-12-11 13:09:14 -0600

    DA

--- resources/views/vitalicia/modnpacientes.blade.php
+++ resources/views/vitalicia/modnpacientes.blade.php
@@ -63,23 +63,23 @@
 
                 <b>Hora de la Actividad 1</b>
             <br>{!! $errors->first('hora1','<span class=error>:message</span>')!!}
-                <input type="time" name="hora1" value="{{old('hora1')}}">
+                <input type="time" name="hora1" value="{{$mnpacientes->hora1}}">
 
                 <b>Actividad 2</b>
             <br>{!! $errors->first('actividad2','<span class=error>:message</span>')!!}
-                <input type="text" name="actividad2" value="{{old('actividad2')}}">
+                <input type="text" name="actividad2" value="{{$mnpacientes->actividad2)}}">
 
                 <b>Hora de la Actividad 2</b>
             <br>{!! $errors->first('hora2','<span class=error>:message</span>')!!}
-                <input type="time" name="hora2" value="{{old('hora2')}}">
+                <input type="time" name="hora2" value="{{$mnpacientes->hora2}}">
 
                 <b>Actividad 3</b>
             <br>{!! $errors->first('actividad3','<span class=error>:message</span>')!!}
-                <input type="text" name="actividad3" value="{{old('actividad3')}}">
+                <input type="text" name="actividad3" value="{{$mnpacientes->actividad3}}">
 
                 <b>Hora de la Actividad 3</b>
             <br>{!! $errors->first('hora3hora3','<span class=error>:message</span>')!!}
-                <input type="time" name="hora3" value="{{old('hora3')}}">
+                <input type="time" name="hora3" value="{{$mnpacientes->hora3}}">
 
             
                 
@@ -90,19 +90,19 @@
 @section('form2')               
                 <b>Comida (Men&uacute;) </b>
             <br>{!! $errors->first('menu','<span class=error>:message</span>')!!}
-                <input type="text" name="menu" value="{{old('menu')}}">
+                <input type="text" name="menu" value="{{$mnpacientes->menu}}">
             
                 <b>Consumo</b>
             <br>{!! $errors->first('consumo','<span class=error>:message</span>')!!}
-                <input type="text" name="consumo" value="{{old('consumo')}}">
+                <input type="text" name="consumo" value="{{$mnpacientes->consumo}}">
                 
                 <b>Observaciones</b> 
             <br>{!! $errors->first('observaciones','<span class=error>:message</span>')!!}
-                <input type="text" name="observaciones" value="{{old('observaciones')}}">
+                <input type="text" name="observaciones" value="{{$mnpacientes->observaciones}}">
 
                 <b>Hora de Comida</b>
             <br>{!! $errors->first('horacomida','<span class=error>:message</span>')!!}
-                <input type="time" name="horacomida" value="{{old('horacomida')}}">
+                <input type="time" name="horacomida" value="{{$mnpacientes->horacomida}}">
             
                 <b>Tipo de Comida</b> 
             @if($errors->first('tipocomida')) 
@@ -150,41 +150,41 @@
                 
                 <b>ta</b>
             <br>{!! $errors->first('ta','<span class=error>:message</span>')!!}
-                <input type="text" name="ta" value="{{old('ta')}}">
+                <input type="text" name="ta" value="{{$mnpacientes->ta}}">
             
                 <b>fc</b>
             <br>{!! $errors->first('fc','<span class=error>:message</span>')!!}
-                <input type="text" name="fc" value="{{old('fc')}}">
+                <input type="text" name="fc" value="{{$mnpacientes->fc}}">
 @stop
 @section('form3')                
                         
                 <b>fr</b>
             <br>{!! $errors->first('fr','<span class=error>:message</span>')!!}
-                <input type="text" name="fr" value="{{old('fr')}}">
+                <input type="text" name="fr" value="{{$mnpacientes->fr}}">
                 
                 <b>Temp</b>
             <br>{!! $errors->first('temp','<span class=error>:message</span>')!!}
-                <input type="text" name="temp" value="{{old('temp')}}">
+                <input type="text" name="temp" value="{{$mnpacientes->temp}}">
                 
                 <b>Spo2</b> 
             <br>{!! $errors->first('spo2','<span class=error>:message</span>')!!}
-                <input type="text" name="spo2" value="{{old('spo2')}}">
+                <input type="text" name="spo2" value="{{$mnpacientes->spo2}}">
             
                 <b>Glucosa</b>
             <br>{!! $errors->first('glucosa','<span class=error>:message</span>')!!}
-                <input type="text" name="glucosa" value="{{old('glucosa')}}">
+                <input type="text" name="glucosa" value="{{$mnpacientes->glucosa}}">
 
                 <b>Pr&oacute;tesis</b>
             <br>{!! $errors->first('protesis','<span class=error>:message</span>')!!}
-                <input type="text" name="protesis" value="{{old('protesis')}}">
+                <input type="text" name="protesis" value="{{$mnpacientes->protesis}}">
                 
                 <b>Nombre del Cuidador</b>
             <br>{!! $errors->first('cuidadornombre','<span class=error>:message</span>')!!}
-                <input type="text" name="cuidadornombre" value="{{old('cuidadornombre')}}">
+                <input type="text" name="cuidadornombre" value="{{$mnpacientes->cuidadornombre}}">
                 
                 <b>Fecha de Registro </b>
             <br>{!! $errors->first('fechacuidador','<span class=error>:message</span>')!!}
-                <input type="date" name="fechacuidador" value="{{old('fechacuidador')}}">
+                <input type="date" name="fechacuidador" value="{{$mnpacientes->fechacuidador}}">
 
         <input type="submit" class="button" value="Guardar">
         <input type="reset" class="button alert" value="Borrar">
