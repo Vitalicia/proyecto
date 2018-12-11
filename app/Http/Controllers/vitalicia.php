5e56d4c48a8401887a58cf1b75e2eb2ef3ec8e84
BrayanCorpse <al221610173@gmail.com>
2018-12-11 13:17:44 -0600

    inp

--- app/Http/Controllers/vitalicia.php
+++ app/Http/Controllers/vitalicia.php
@@ -1324,7 +1324,7 @@ public function guardamodifinpacientes(Request $request)
             
 
                   $npacm = npacientes::find($idnp);
-                
+                  $npacm->idnp = $request->idnp;
                   $npacm->actividad1 = $request->actividad1;    
                   $npacm->hora1 = $request->hora1;
                   $npacm->actividad2 = $request->actividad2;
