dc22e94b93480d6d587f63f6924b828c5fe9d065
BrayanCorpse <al221610173@gmail.com>
2018-12-12 19:07:53 -0600

    a

--- app/Http/Controllers/vitalicia.php
+++ app/Http/Controllers/vitalicia.php
@@ -1270,13 +1270,16 @@ public function eliminausu($idu)
                     
                         $otromedi = amedicamentos::where('idamedicamento','=',$idamedicamento)->get(); 
 
-                
+                        $otro = amedicamentos::where('idamedicamento','!=',$idamedicamento)->get(); 
+
 		
 		        return view ('vitalicia.modnpacientes')
 		        ->with('mnpacientes',$mnpacientes[0])
-	                ->with('idu',$idu);
-                      
-                    
+                        ->with('idu',$idu)
+                        ->with('otrousario',$otrousario)
+                        ->with('otromedi',$otromedi)
+                        ->with('idamedicamento',$idamedicamento)
+                        ->with('otro',$otro[0]->nmedica);
                         
                         
 
