ffa7164e86c44df6ad04181928426b0d66be8bde
BrayanCorpse <al221610173@gmail.com>
2018-12-12 18:02:24 -0600

    a

--- app/npacientes.php
+++ app/npacientes.php
@@ -9,7 +9,7 @@ class npacientes extends Model
 {
 	use SoftDeletes;
     protected $primaryKey ='idnp';
-    protected $fillable = ['idnp','actividad1','hora1','actividad2','hora2','actividad3','hora3','menu','consumo','observaciones','horacomida','tipocomida','tgeriatrico1','tgeriatrico2','tgeriatrico3','ta','fc','fr','temp','spo2','glucosa','protesis','cuidadornombre','fechacuidador','idu','amindicacion','ampresen'];
+    protected $fillable = ['idnp','actividad1','hora1','actividad2','hora2','actividad3','hora3','menu','consumo','observaciones','horacomida','tipocomida','tgeriatrico1','tgeriatrico2','tgeriatrico3','ta','fc','fr','temp','spo2','glucosa','protesis','cuidadornombre','fechacuidador','idu','idamedicamento','amindicacion','ampresen'];
     protected $date=['deleted_at'];
 
 }
