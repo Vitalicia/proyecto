f0e9608e2562e999f3bc0ad5dbce02765f1a0e4c
BrayanCorpse <al221610173@gmail.com>
2018-12-12 22:07:56 -0600

    m

--- resources/views/vitalicia/mpacientes.blade.php
+++ resources/views/vitalicia/mpacientes.blade.php
@@ -15,13 +15,15 @@
 
 
         <b>Nombre</b>
-        <select name = 'idd'>
-      <option value = '{{$idd}}'>{{$otrod}}</option>
+  
 	  @foreach($otrodato as $od)
-	   <option value = '{{$od->idd}}'>{{$od->nombre}}</option>
-	  @endforeach
-      </select>
-      <br>
+    <input type ="text" name="nombre" value='{{$od->nombre}}'>
+    @endforeach
+      
+      
+
+        
+
       
         <input type="submit" class="button" value="Guardar">
         <input type="reset" class="button alert" value="Borrar">
